202205b9c594b72208bfa8fec5a533a855bd6da1
CameronCSS <Cameronseamons@gmail.com>
2023-01-13 23:41:37 -0700

    deploy react build

---
+++ src/data.js
@@ -0,0 +1,127 @@
+// Skills icons - https://react-icons.github.io/react-icons/
+import {
+  FaHtml5,
+  FaPython,
+  FaReact,
+  FaGithubSquare,
+  FaLinkedin,
+} from "react-icons/fa";
+import { SiBlender, SiMysql, SiPowerbi } from "react-icons/si";
+
+// Projects Images (add your images to the images directory and import below)
+import Data from "./images/Data.jpg";
+import SQL from "./images/SQL.PNG";
+import Programming from "./images/Programming.PNG";
+import Profile from "./images/Profile.PNG";
+import Logo from "./images/logo.svg";
+
+// Hero Images (add your images to the /images directory with the same names)
+import Light from "./images/hero-light.jpg";
+import Dark from "./images/hero-dark.jpg";
+// If you change the import names above then you need to change the export names below
+export { Light };
+export { Dark };
+
+/* START HERE
+ **************************************************************
+  Add your GitHub username (string - "YourUsername") below.
+*/
+export const githubUsername = "CameronCSS";
+
+// Blog link icon (imported above)
+export const Blog = <FaLinkedin />;
+
+/* Skills
+ ************************************************************** 
+  Add or remove skills in the SAME format below, there must be one icon imported above per skill below.
+*/
+export const skillData = [
+  {
+    id: 1,
+    skill: <SiMysql className="display-4" />,
+    name: "MySQL",
+  },
+  {
+    id: 2,
+    skill: <FaPython className="display-4" />,
+    name: "Python",
+  },
+  {
+    id: 3,
+    skill: <SiPowerbi className="display-4" />,
+    name: "PowerBI",
+  },
+  {
+    id: 4,
+    skill: <SiBlender className="display-4" />,
+    name: "Blender",
+  },
+  {
+    id: 5,
+    skill: <FaReact className="display-4" />,
+    name: "React",
+  },
+  {
+    id: 6,
+    skill: <FaHtml5 className="display-4" />,
+    name: "HTML5",
+  },
+  {
+    id: 7,
+    skill: <FaGithubSquare className="display-4" />,
+    name: "GitHub",
+  },
+];
+
+// Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
+export const resume = null;
+
+/* Projects
+ ************************************************************** 
+  List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
+*/
+export const filteredProjects = ["SQL-Projects", "Data-Analysis", "Programming-Projects"];
+
+// Replace the defualt GitHub image for matching repos below (images imported above - lines 13-14)
+export const projectCardImages = [
+  {
+    name: "Data-Analysis",
+    image: Data,
+  },
+  {
+    name: "SQL-Projects",
+    image: SQL,
+  },
+  {
+    name: "Programming-Projects",
+    image: Programming,
+  },
+  {
+    name: "CameronCSS",
+    image: Profile,
+  },
+  {
+    name: "CameronCSS.github.io",
+    image: Logo,
+  },
+  {
+    name: "react-deploy",
+    image: Logo,
+  },
+
+];
+
+/* About Me
+ **************************************************************
+  Add a second paragraph for the about me section.
+*/
+export const moreInfo =
+  "A data professional with a passion for visualization and storytelling, my goal is to utilize my skills in SQL, Power BI, and Python to turn complex data sets into easily understandable insights.";
+
+
+/* Contact Info
+ ************************************************************** 
+  Add your formspree endpoint below.
+  https://formspree.io/
+*/
+export const formspreeUrl = "https://formspree.io/f/xayznwzp";
